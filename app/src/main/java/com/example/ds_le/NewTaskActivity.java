7b7daeccde694b436e2f909ac97ce412dc94e2c1
JaimeLucero <jaimeemanuellucero@gmail.com>
2023-12-14 20:19:45 +0800

    Add task UI

---
+++ app/src/main/java/com/example/ds_le/NewTaskActivity.java
@@ -0,0 +1,13 @@
+package com.example.ds_le;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
+}
